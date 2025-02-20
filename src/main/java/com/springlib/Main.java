50e094bb4607fd388bb4d8a8a5961e9df4c992eb
Balmukund Pandey <smpandey.2008@gmail.com>
2025-02-20 22:58:33 +0530

    Docker Java

---
+++ src/main/java/com/springlib/Main.java
@@ -0,0 +1,9 @@
+package com.springlib;
+
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Current Time : "+ LocalDateTime.now());
+    }
+}
